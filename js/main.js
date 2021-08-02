52353d22189d77ca62707b21efc34b5d5f0b9dd2
anaccenci <cencianacaroline@gmail.com>
2021-08-01 22:50:47 -0300

    criei a comida da cobrinha.

--- js/main.js
+++ js/main.js
@@ -10,6 +10,10 @@ snake[0] = {
 
 }
 let direction = "right";
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
 
 function criarBG() {
     context.fillStyle = "lightgreen";
@@ -23,6 +27,11 @@ function criarCobrinha() {
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 
+}
+
+function drawFood() {
+    context.fillStyle = "#FF33BE";
+    context.fillRect(food.x, food.y, box, box);
 }
 
 document.addEventListener('keydown', update);
@@ -41,6 +50,7 @@ function inciarJogo() {
     if (snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
     criarBG();
     criarCobrinha();
+    drawFood();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
